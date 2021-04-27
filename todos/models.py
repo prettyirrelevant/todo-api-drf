6df257c0716296129aa8b25bce2953a98a300a3a
Adewumi Isaac <ienioladewumi@gmail.com>
2021-04-27 17:53:15 +0100

    feat: added `todo` model and made migrations

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    uuid = models.UUIDField("uuid", default=uuid.uuid4, editable=False)
+    content = models.CharField("Content", max_length=150, blank=False, null=False)
+    is_complete = models.BooleanField("Is Complete", default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
