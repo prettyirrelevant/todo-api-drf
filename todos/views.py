51d7aff6cfb7633411bc62dc4ba619f881e2764e
Adewumi Isaac <ienioladewumi@gmail.com>
2021-04-27 18:03:25 +0100

    feat: added `todo` create endpoint

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoCreateView(CreateAPIView):
+    """
+    Creates new todo.
+    """
+
+    queryset = Todo
+    serializer_class = TodoSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(
+            data={"message": "Todo created successfully!", "data": serializer.data},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
